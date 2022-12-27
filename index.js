b0058a19438dfde461da8b058a64246aac161b8d
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2022-12-27 16:04:19 +0200

    Add express

---
+++ index.js
@@ -0,0 +1,9 @@
+let colors = require('colors');
+let express = require('express');
+let app = express();
+
+app.use(express.static('static'));
+
+const portNumber = 3000;
+let server = app.listen(portNumber,
+    () => console.log('Listening on port '.yellow + `${portNumber}`.green));
