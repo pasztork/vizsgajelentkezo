7c791783deeb5c029e025328eda1d5fa30049d2e
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-01-15 14:22:14 +0100

    add login form

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ let app = express();
 app.use(express.static('static'));
 
 const portNumber = 3000;
-let server =    app.listen(portNumber,
+let server = app.listen(portNumber,
     () => console.log('Listening on port '.yellow + `${portNumber}`.green)
 );
