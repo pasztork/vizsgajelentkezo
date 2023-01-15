c6217bfbcf22fb8c5875238c5e040ea0312cd1ed
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-01-15 18:20:49 +0100

    remove unnecessary code

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ require('./routes/login')(app);
 app.use(express.static('static'));
 
 const portNumber = 3000;
-let server = app.listen(portNumber,
+app.listen(portNumber,
     () => console.log('Listening on port '.yellow + `${portNumber}`.green)
 );
